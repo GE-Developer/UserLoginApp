0ecc1accfcdbc5d53a4c24e5586ceed37919742f
GE-Developer <on-line-misha@mail.ru>
2023-03-31 03:34:09 +0400

    Classes added

---
+++ UserLoginApp/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  UserLoginApp
+//
+//  Created by M I C H A E L on 31.03.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
--- UserLoginApp/WelcomeViewController.swift
+++ UserLoginApp/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  UserLoginApp
 //
 //  Created by M I C H A E L on 31.03.2023.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
